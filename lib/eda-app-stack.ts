12977630090a4b2af4c93f53f1b279a8086b53d3
Maxamed Maxamed <20088349@mail.wit.ie>
2024-05-23 11:31:08 +0100

    Question 2 architecture

--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -1,71 +1,58 @@
 import * as cdk from "aws-cdk-lib";
 import * as lambdanode from "aws-cdk-lib/aws-lambda-nodejs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
-import * as s3 from "aws-cdk-lib/aws-s3";
-import * as s3n from "aws-cdk-lib/aws-s3-notifications";
-import * as events from "aws-cdk-lib/aws-lambda-event-sources";
 import * as sqs from "aws-cdk-lib/aws-sqs";
 import * as sns from "aws-cdk-lib/aws-sns";
 import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
-import * as iam from "aws-cdk-lib/aws-iam";
-import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as events from "aws-cdk-lib/aws-lambda-event-sources";
 
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class EDAAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-	super(scope, id, props);
+    super(scope, id, props);
 
-	const bucket = new s3.Bucket(this, "ABucket", {
-  	removalPolicy: cdk.RemovalPolicy.DESTROY,
-  	autoDeleteObjects: true,
-  	publicReadAccess: false,
-	});
+    const queueA = new sqs.Queue(this, "QueueA", {
+      receiveMessageWaitTime: cdk.Duration.seconds(5),
+    });
 
-	const table = new dynamodb.Table(this, "TableA", {
-  	billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-  	partitionKey: { name: "name", type: dynamodb.AttributeType.STRING },
-  	removalPolicy: cdk.RemovalPolicy.DESTROY,
-  	tableName: "ZppData",
-	});
+    const queueB = new sqs.Queue(this, "QueueB", {
+      receiveMessageWaitTime: cdk.Duration.seconds(10),
+    });
 
-	const queueB = new sqs.Queue(this, "QueueB", {
-  	receiveMessageWaitTime: cdk.Duration.seconds(10),
-	});
+    const topic1 = new sns.Topic(this, "TopicA", {
+      displayName: "New Image topic",
+    });
 
-	const queueA = new sqs.Queue(this, "QueueA", {
-  	receiveMessageWaitTime: cdk.Duration.seconds(5),
-	});
+    const topic2 = new sns.Topic(this, "TopicB", {
+      displayName: "New Image topic",
+    });
 
-	const topic1 = new sns.Topic(this, "TopicA", {
-  	displayName: "New Image topic",
-	});
+    // Add subscription with filter policy to Topic1
+    topic1.addSubscription(new subs.SqsSubscription(queueA, {
+      filterPolicy: {
+        country: sns.SubscriptionFilter.stringFilter({
+          allowlist: ["Ireland", "Spain"],
+        }),
+      },
+    }));
 
-	const topic2 = new sns.Topic(this, "TopicB", {
-  	displayName: "New Image topic",
-	});
+    const lambdaAFn = new lambdanode.NodejsFunction(this, "lambdaAFn", {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      entry: `${__dirname}/../lambdas/lambdaA.ts`,
+      timeout: cdk.Duration.seconds(15),
+      memorySize: 128,
+    });
 
-	const lambdaAFn = new lambdanode.NodejsFunction(this, "lambdaAFn", {
-  	runtime: lambda.Runtime.NODEJS_18_X,
-  	entry: `${__dirname}/../lambdas/lambdaA.ts`,
-  	timeout: cdk.Duration.seconds(15),
-  	memorySize: 128,
-	});
+    lambdaAFn.addEventSource(new events.SqsEventSource(queueA));
 
-	lambdaAFn.addEventSource(new events.SnsEventSource(topic1));
+    const lambdaBFn = new lambdanode.NodejsFunction(this, "lambdaBFn", {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      entry: `${__dirname}/../lambdas/lambdaB.ts`,
+      timeout: cdk.Duration.seconds(15),
+      memorySize: 128,
+    });
 
-	const lambdaBFn = new lambdanode.NodejsFunction(this, "lambdaBFn", {
-  	runtime: lambda.Runtime.NODEJS_18_X,
-  	entry: `${__dirname}/../lambdas/lambdaB.ts`,
-  	timeout: cdk.Duration.seconds(15),
-  	memorySize: 128,
-	});
-
-	lambdaBFn.addEventSource(new events.SqsEventSource(queueB));
-
-	new cdk.CfnOutput(this, "Topic 1", {
-  	value: topic1.topicArn,
-	});
+    lambdaBFn.addEventSource(new events.SqsEventSource(queueB));
   }
 }
